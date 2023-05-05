15f7a96937884bdd4b9fb40d854168f118d07bd6
Noa Leon <n.leon.carrasco@gmail.com>
2023-05-05 09:17:06 +0200

    started routing for posts

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User.model');
+const { isAuthenticated } = require('../middleware/jwt.middleware.js');
+
+const Post = require('../models/Post.model')
+
 router.post('/create', (req, res) => {
   // const body = req.body;
+  const { description, image } = req.body
 
-  console.log(req.body);
+  if (description === '' || image === '' ) {
+    res.sendStatus(400).json({ message: 'Provide a description and image' });
+    return;
+  }
 
-  res.send(201);
+  console.log(info);
+
+  res.sendStatus(201);
 });
 
 module.exports = router;
