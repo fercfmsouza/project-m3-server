44f7c9e5c4ef2def1d3bb669b68688f4cccf5287
Noa Leon <n.leon.carrasco@gmail.com>
2023-05-18 13:38:07 +0200

    settings routing - trying to edit the user.email

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -23,10 +23,31 @@ router.get('/:id', isAuthenticated, async (req, res) => {
 
 //settings
 router.post('/settings', isAuthenticated, async (req, res) => {
+  const { id } = req.params;
   const user = req.payload;
 
   try {
+    const user = await User.findById(id);
+    await user.populate('posts');
+
     res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+// edit user.email
+router.put('/:id', isAuthenticated, async (req, res) => {
+  const user = req.payload;
+
+  console.log('body', req.body);
+  console.log('user', req.payload);
+
+  try {
+     await User.findByIdAndUpdate(user._id, req.body);
+
+     res.sendStatus(200);
   } catch (error) {
     console.error(error);
     res.status(500).send(error);
