41fbdf362736392333b28a03e2763a129721f391
Noa Leon <n.leon.carrasco@gmail.com>
2023-05-18 18:08:29 +0200

    working on password reset

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const app = require("./app");
+const connection = require("./db/reset.js");
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
+
+connection();
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
